2ac7239ae2291fb0d8aa8561f2fd0511342284f0
carrascomj <carrascomurielj@gmail.com>
2020-03-30 18:46:57 +0200

    feat: receive post request, image malformed

--- static/main.js
+++ static/main.js
@@ -1,6 +1,8 @@
 var target_brows = document.getElementById("fileb");
+var img1 = document.getElementById("img1");
 
 target_brows.addEventListener('change', loadDoc);
+img1.addEventListener('change', () => {console.log("hall1");});
 
 // check type
 var fileTypes = [
@@ -20,24 +22,36 @@ function validFileType(file) {
 }
 
 // POST REQUEST TO SERVER: upload image
-function loadDoc() {
-  console.log("Yes!");
-  var input = target_brows,
-  file = input.files[0];
+function loadDoc(event) {
+  // cancel default actions
+  // event.preventDefault();
+  var file = target_brows.files[0];
+  if (validFileType(file)) {
+    var fr = new FileReader();
+    fr.onloadend = function () {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "/img_upload");
+      console.log(fr.result);
+      xhr.onload = function(e) {
+        //The response of the upload
+        xhr.responseText;
+        if(xhr.status === 200) {
+          console.log("Received stuff");
+          var data = JSON.parse(xhr.responseText);
+          img1.src = "data:image/png;base64," + data.images[0];
+        } else {
+          console.log("Some error:");
+          console.log(xhr.responseText);
+        }
+      }
 
-  var fd = new FormData();
-  fd.append("data", file);
-  fd.append("complete", true);
-  fd.append("boundary", "huhuhpuche");
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "/img_upload");
-  xhr.send(fd);
-  xhr.onloadend = function(e) {
-      //The response of de upload
-      xhr.responseText;
-      if(callback) {
-        callback();
-  }
+      xhr.send(JSON.stringify({
+        "file_type": file.type,
+        "image": fr.result.slice((fr.result.indexOf("base64")+7),-1),
+        // "image": fr.result,
+        "message": "Apples",
+      }));
+    }
+    fr.readAsDataURL(file);
   }
 }
