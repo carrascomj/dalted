db3b49aa6cce8f0cb7dd437a169dc964ffe61333
carrascomj <carrascomurielj@gmail.com>
2020-04-06 01:46:47 +0200

    fix: correct typo on matmul

--- src/image_proocessing/pipe_transform.rs
+++ src/image_proocessing/pipe_transform.rs
@@ -6,8 +6,6 @@ use imageproc::map::map_colors;
 use std::io::Cursor;
 
 pub fn pipe_matrix_multiplication(raw_data: String) -> Vec<String> {
-    // let mut reader = Reader::new(Cursor::new(decode(raw_data).unwrap()));
-    // reader.set_format(ImageFormat::Png);
     let reader = Reader::new(Cursor::new(decode(raw_data).unwrap()))
         .with_guessed_format()
         .expect("hhh.");
@@ -27,9 +25,9 @@ fn color_filter(img: &DynamicImage, filter: &KERNEL) -> String {
         let g = p[1] as f32;
         let b = p[2] as f32;
         Rgb([
-            (filter[0] * r) as u8 + (filter[1] + g) as u8 + (filter[2] * b) as u8,
-            (filter[3] * r) as u8 + (filter[4] + g) as u8 + (filter[5] * b) as u8,
-            (filter[6] * r) as u8 + (filter[7] + g) as u8 + (filter[8] * b) as u8,
+            (filter[0] * r + filter[1] * g + filter[2] * b) as u8,
+            (filter[3] * r + filter[4] * g + filter[5] * b) as u8,
+            (filter[6] * r + filter[7] * g + filter[8] * b) as u8,
         ])
     }))
     .write_to(&mut image_png, image::ImageOutputFormat::Png).unwrap();
