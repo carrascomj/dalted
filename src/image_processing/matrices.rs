8d8a19bc888c9fbb7de3fb83d5f5d3ce25c494b9
carrascomj <carrascomurielj@gmail.com>
2020-04-10 15:34:12 +0200

    tests: add unit tests for matrix operations

--- src/image_processing/matrices.rs
+++ src/image_processing/matrices.rs
@@ -39,6 +39,26 @@ impl<T> Kernel<T> {
     }
 }
 
+// dot product
+impl<T: Mul<T, Output = T> + Add<T, Output = T> + Copy> Mul for Kernel<T> {
+    type Output = Kernel<T>;
+
+    fn mul(self, rhs: Kernel<T>) -> Self {
+        let rhs = rhs.matrix;
+        Kernel::<T>::new([
+            self.matrix[0] * rhs[0] + self.matrix[1] * rhs[3] + self.matrix[2] * rhs[6],
+            self.matrix[0] * rhs[1] + self.matrix[1] * rhs[4] + self.matrix[2] * rhs[7],
+            self.matrix[0] * rhs[2] + self.matrix[1] * rhs[5] + self.matrix[2] * rhs[8],
+            self.matrix[3] * rhs[0] + self.matrix[4] * rhs[3] + self.matrix[5] * rhs[6],
+            self.matrix[3] * rhs[1] + self.matrix[4] * rhs[4] + self.matrix[5] * rhs[7],
+            self.matrix[3] * rhs[2] + self.matrix[4] * rhs[5] + self.matrix[5] * rhs[8],
+            self.matrix[6] * rhs[0] + self.matrix[7] * rhs[3] + self.matrix[8] * rhs[6],
+            self.matrix[6] * rhs[1] + self.matrix[7] * rhs[4] + self.matrix[8] * rhs[7],
+            self.matrix[6] * rhs[2] + self.matrix[7] * rhs[5] + self.matrix[8] * rhs[8],
+        ])
+    }
+}
+
 #[derive(Debug)]
 pub struct Vec3<T> {
     cont: [T; 3],
@@ -53,8 +73,8 @@ impl<T> Vec3<T> {
     }
 }
 
-impl<T: std::convert::From<u8>> From<[u8;4]> for Vec3<T>{
-    fn from(tu: [u8;4]) -> Self  {
+impl<T: std::convert::From<u8>> From<[u8; 4]> for Vec3<T> {
+    fn from(tu: [u8; 4]) -> Self {
         Vec3::new([T::from(tu[0]), T::from(tu[1]), T::from(tu[2])])
     }
 }
@@ -96,5 +116,45 @@ impl<T: Copy + Mul<T, Output = T> + Add<T, Output = T>> Matops3<T> for Vec3<T> {
     }
     fn apply(&self, f: impl Fn(T) -> T) -> Self {
         Vec3::new([f(self.cont[0]), f(self.cont[1]), f(self.cont[2])])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn dot_product() {
+        let ex_k = Kernel::new([1u8, 2, 3, 4, 5, 6, 7, 8, 9]);
+        let ex_s = Kernel::new([9u8, 2, 3, 0, 5, 6, 1, 8, 9]);
+        assert_eq!(
+            Kernel::new([12, 36, 42, 42, 81, 96, 72, 126, 150]).matrix,
+            (ex_k * ex_s).matrix
+        );
+    }
+
+    #[test]
+    fn hadamart_product() {
+        let ex_k = Vec3::new([1u8, 2, 3]);
+        let ex_s = Vec3::new([9u8, 2, 3]);
+        assert_eq!(Vec3::new([9, 4, 9]).cont, (ex_k.vecmul(ex_s)).cont);
+    }
+
+    #[test]
+    fn dot_product_kernel_x_vec() {
+        let ex_k = Kernel::new([1f32, 2., 3., 4., 5., 6., 7., 8., 9.]);
+        assert_eq!(
+            Vec3::new([36., 84., 132.]).cont,
+            ex_k.vecmul(Vec3::new([5., 2., 9.])).cont
+        );
+    }
+
+    #[test]
+    fn apply_mul() {
+        let ex_k = Kernel::new([1., 2., 3., 4., 5., 6., 7., 8., 9.]);
+        assert_eq!(
+            Kernel::new([5., 10., 15., 20.0, 25.0, 30.0, 35.0, 40.0, 45.0]).matrix,
+            ex_k.apply(|x| x * 5.).matrix
+        );
     }
 }
--- src/image_processing/pipe_transform.rs
+++ src/image_processing/pipe_transform.rs
@@ -1,4 +1,4 @@
-use crate::image_processing::matrices::{Kernel, Vec3, Matops3, MATRICES};
+use crate::image_processing::matrices::{Kernel, Matops3, Vec3, MATRICES};
 use base64::{decode, encode};
 use image::io::Reader;
 use image::{DynamicImage, Rgb};
@@ -13,8 +13,7 @@ use std::io::Cursor;
 /// - Vietnol el al, 1999 http://vision.psychol.cam.ac.uk/jdmollon/papers/colourmaps.pdf
 /// - Explanatory post https://ixora.io/projects/colorblindness/color-blindness-simulation-research/
 pub fn pipe_matrix_multiplication(raw_data: String) -> Result<Vec<String>, Box<dyn Error>> {
-    let reader = Reader::new(Cursor::new(decode(raw_data)?))
-        .with_guessed_format()?;
+    let reader = Reader::new(Cursor::new(decode(raw_data)?)).with_guessed_format()?;
     let img = reader.decode()?;
 
     let mut transformed: Vec<String> = vec![];
@@ -25,7 +24,7 @@ pub fn pipe_matrix_multiplication(raw_data: String) -> Result<Vec<String>, Box<d
 }
 
 /// Tranform RGB values in linear space [0, 1] with a matrix and return normal RGB values [0, 255]
-fn color_filter(img: &DynamicImage, matrix: &Kernel<f32>) -> Result<String, Box<dyn Error>> {
+pub fn color_filter(img: &DynamicImage, matrix: &Kernel<f32>) -> Result<String, Box<dyn Error>> {
     let mut image_png = Vec::<u8>::new();
     DynamicImage::ImageRgb8(map_colors(img, |p| {
         let v = matrix
@@ -60,5 +59,26 @@ fn gamma_correction(rgb_linear: f32) -> f32 {
         255.0
     } else {
         res
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::image_processing::matrices::Kernel;
+
+    #[test]
+    fn test_color_filter() {
+        let ground = "iVBORw0KGgoAAAANSUhEUgAAAAIAAAACCAIAAAD91JpzAAAAFUlEQVR4nGMUVNB6/+AaIwMDA5AFABzRA177FJQYAAAAAElFTkSuQmCC";
+        let raw_data = b"P1 2 2\n\
+            0 1\n\
+            1 0\n";
+        let rgb = Reader::new(Cursor::new(raw_data))
+            .with_guessed_format()
+            .unwrap()
+            .decode()
+            .unwrap();
+        let kernel = Kernel::new([1., 2., 3., 4., 5., 6., 7., 8., 9.]);
+        assert_eq!(color_filter(&rgb, &kernel).unwrap(), ground)
     }
 }
