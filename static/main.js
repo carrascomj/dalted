60b67f91c5f6a42a935129686151f6af9d217044
carrascomj <carrascomurielj@gmail.com>
2020-04-14 16:11:36 +0200

    refactor: improve picture-based communication

--- static/main.js
+++ static/main.js
@@ -42,13 +42,7 @@ target_drag.addEventListener(
 );
 
 // check type
-var fileTypes = [
-  "image/jpeg",
-  "image/jpg",
-  "image/pjpeg",
-  "image/png",
-  "image/svg"
-];
+var fileTypes = ["image/jpeg", "image/jpg", "image/png"];
 
 function validFileType(file) {
   for (var i = 0; i < fileTypes.length; i++) {
@@ -65,20 +59,9 @@ function loadDoc(target) {
   // event.preventDefault();
   var file = target.files[0];
   if (validFileType(file)) {
-    // clear previous images...
-    img1.src = "";
-    img2.src = "";
-    img3.src = "";
-    img4.src = "";
-    img5.src = "";
-    img6.src = "";
-    // ...and let the user know that things are moving
-    img1.alt = "Transforming...";
-    img2.alt = "Transforming...";
-    img3.alt = "Transforming...";
-    img4.alt = "Transforming...";
-    img5.alt = "Transforming...";
-    img6.alt = "Transforming...";
+    // clear previous images and let the user know that things are moving
+    fill_pics("Transforming...");
+
     var fr = new FileReader();
     fr.onloadend = function() {
       var xhr = new XMLHttpRequest();
@@ -89,11 +72,13 @@ function loadDoc(target) {
         if (xhr.status === 200) {
           var data = JSON.parse(xhr.responseText);
           let original = fr.result;
-          if (file.type === "image/jpeg") {
+          if (file.type === "image/jpg") {
             original =
-              "data:image/png;base64," +
+              "data:image/jpg;base64," +
               original.slice(original.indexOf("base64") + 7, -1);
           }
+          // sometimes "normal" is not generated for jpg
+          img1.alt = "normal";
           img1.src = original;
           img2.src = "data:image/png;base64," + data.images[0];
           img3.src = "data:image/png;base64," + data.images[1];
@@ -101,12 +86,10 @@ function loadDoc(target) {
           img5.src = "data:image/png;base64," + data.images[3];
           img6.src = "data:image/png;base64," + data.images[4];
         } else {
-          img1.alt = "Image could not be parsed";
-          img3.alt = xhr.responseText;
-          img2.alt = "Reason ----------------------->";
-          img4.alt = "Is image too large?";
-          img5.alt = "Supported: PNG, JPG, JPEG";
-          img6.alt = "Supported: PNG, JPG, JPEG";
+          fill_pics("Is image too large?");
+          img4.alt = "Image could not be parsed";
+          img5.alt = "Error message ->";
+          img6.alt = xhr.responseText;
         }
       };
 
@@ -120,5 +103,28 @@ function loadDoc(target) {
       );
     };
     fr.readAsDataURL(file);
+  } else {
+    fill_pics("Supported: PNG, JPG, JPEG");
+
+    img1.alt = "format NOT supported";
+    img2.alt = `input type: ${file.type}`;
+  }
+}
+
+function fill_pics(filler = "") {
+  img1.src = "";
+  img2.src = "";
+  img3.src = "";
+  img4.src = "";
+  img5.src = "";
+  img6.src = "";
+  if (filler !== "") {
+    img6.src = "";
+    img1.alt = filler;
+    img2.alt = filler;
+    img3.alt = filler;
+    img4.alt = filler;
+    img5.alt = filler;
+    img6.alt = filler;
   }
 }
